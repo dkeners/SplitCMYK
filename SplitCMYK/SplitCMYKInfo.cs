e861143852c9a4a2c740074e53515e694ede100c
dkeners <dfrostk06@gmail.com>
2024-02-10 23:16:10 +0100

    UpdatedInfo

--- SplitCMYK/SplitCMYKInfo.cs
+++ SplitCMYK/SplitCMYKInfo.cs
@@ -10,17 +10,17 @@ namespace SplitCMYK
     public override string Name => "SplitCMYK";
 
     //Return a 24x24 pixel bitmap to represent this GHA library.
-    public override Bitmap Icon => null;
+    public override Bitmap Icon => Properties.Resources.CMYK;
 
     //Return a short string describing the purpose of this GHA library.
-    public override string Description => "";
+    public override string Description => "Gets CMYK values from a Grasshopper Colour datatype.";
 
     public override Guid Id => new Guid("4809ab0b-5ff2-44be-b408-1bbca992108e");
 
     //Return a string identifying you or your company.
-    public override string AuthorName => "";
+    public override string AuthorName => "dkeners";
 
     //Return a string representing your preferred contact details.
-    public override string AuthorContact => "";
+    public override string AuthorContact => "contact@dankenerson.com";
   }
 }
